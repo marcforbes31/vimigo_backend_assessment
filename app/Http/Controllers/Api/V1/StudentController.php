e25404918b2ac956b487322fc5e3f74661b856d3
Marc Forbes <marcforbes31@gmail.com>
2023-01-02 23:49:59 +0800

    Moved Student Store and Update Request to Request\V1 file (forgot to place in there previously hehe)

--- app/Http/Controllers/Api/V1/StudentController.php
+++ app/Http/Controllers/Api/V1/StudentController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Models\Student;
-use App\Http\Requests\StoreStudentRequest;
-use App\Http\Requests\UpdateStudentRequest;
+use App\Http\Requests\V1\StoreStudentRequest;
+use App\Http\Requests\V1\UpdateStudentRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\StudentCollection;
 use App\Http\Resources\V1\StudentResource;
